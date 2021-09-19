d7bbfd09c5d9867f2b5d416db979f60ada170e9d
rabbygit <rabbyhossain482@gmail.com>
2021-09-19 21:59:37 +0600

    ready to deploy

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,20 +1,20 @@
 const Config = function () {
   switch (process.env.NODE_ENV) {
-    case 'production':
-      return {
-        username: 'picktask',
-        dbName: 'floor_map',
-        password: 'e!1btl[3J8$KCm/o',
-        host: '174.138.27.189',
-        dialect: 'mysql',
-      };
-
-    default:
+    case 'dev':
       return {
         username: 'root',
         dbName: 'floor_map',
         password: '',
         host: 'localhost',
+        dialect: 'mysql',
+      };
+
+    default:
+      return {
+        username: 'picktask',
+        dbName: 'floor_map',
+        password: 'e!1btl[3J8$KCm/o',
+        host: '174.138.27.189',
         dialect: 'mysql',
       };
   }
